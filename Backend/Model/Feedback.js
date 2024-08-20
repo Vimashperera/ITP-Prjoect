5fb3c0f7d54ce0e6bf31d622c3dbd81564cd7f41
Sanjaya Jayasinghe <njaya100@gmail.com>
2024-08-20 22:28:41 +0530

    feedback model update

---
+++ Backend/Model/Feedback.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from 'mongoose';
+
+// Defining the Feedback Schema
+const feedbackSchema = new Schema({
+    cusID: {
+        type: String,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone_number: {
+        type: String,
+        required: true,
+    },
+    employee: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    star_rating: {
+        type: Number,
+        required: true,
+    },
+}, { timestamps: true });
+
+// Exporting the Feedback Model
+export const Feedback = mongoose.model('Feedback', feedbackSchema);
