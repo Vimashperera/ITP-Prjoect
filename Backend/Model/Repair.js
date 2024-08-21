375bbcc6f383f1f006d99e7cbaddc135d03720a5
NNanayakkara <nirmaninanayakkara999@gmail.com>
2024-08-21 09:08:02 -0700

    update route and model

---
+++ Backend/Model/Repair.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const repairSchema = mongoose.Schema(
+    {
+        customerName: {
+            type: String,
+            required: true,
+        },
+        customerEmail: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        },
+        customerPhone: {
+            type: String,
+            required: true,
+            unique: true,
+            //match: /^\+\d{1,3}\s\(\d{3}\)\s\d{3}-\d{4}$/,
+        },
+        vehicleMake: {
+            type: String,
+            required: true,
+        },
+        vehicleModel: {
+            type: String,
+            required: true,
+        },
+        vehicleNo: {
+            type: String,
+            required: true,
+        },
+
+        repairDescription: {
+            type: String,
+            required: true,
+        },
+        repairStatus: {
+            type: String,
+            required: true,
+            enum: ["Pending", "In Progress", "Completed"],
+        },Insuranceprovider: {
+            type: String,
+            required: true,
+        },
+        Agent:{
+            type: String,
+            required: true,
+        },
+        });
+
+        export const Repair = mongoose.model('Repair', repairSchema);
+     
--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ import Booking_Route from './Routes/Booking_Route.js';
 //  import Feedback_Route from './Routes/Feedback_Route.js';
 import Inquire_Route from './Routes/inquire_Route.js';
 //  import Promotion_Route from './Routes/Promotion_Route.js';
-
+import Repair_Route from './Routes/Repair_Route.js';
 
 //  import ServiceHistory_Route from './Routes/ServiceHistory_Route.js';
 //  import Store_Route from './Routes/Store_Route.js';
@@ -52,7 +52,7 @@ app.use('/Booking', Booking_Route);
 //  app.use('/Feedback', Feedback_Route);
  app.use('/inquire', Inquire_Route);
 //  app.use('/Promotion', Promotion_Route);
-//  app.use('/Repair', Repair_Route);
+ app.use('/Repair', Repair_Route);
 
 //  app.use('/ServiceHistory', ServiceHistory_Route);
 //  app.use('/Store', Store_Route);
