0b76f7ae37776f6dbfb45d957ad4ae8af3826ae2
svsachi <sachin.vishwa.sv1@gmail.com>
2024-08-23 00:31:25 +0530

    Updated Model

--- Backend/Model/Promotion.js
+++ Backend/Model/Promotion.js
@@ -1,2 +1,30 @@
-//test
-//text2
+import mongoose from "mongoose";
+
+// Defining the Promotion Schema
+const promotionSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    discount: {
+        type: Number,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    }
+}, { timestamps: true });
+
+// Exporting the Promotion Model
+export const Promotion = mongoose.model('Promotion', promotionSchema);
+
+
