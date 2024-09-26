35401fc3af9dd52604c7b345d042d1a5e0259f1b
Vimashperera <vimash2k01@gmail.com>
2024-09-26 16:06:44 +0530

    app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import ShowAllBooking from "./assets/pages/Booking/ShowAllBooking";
+import CreateBooking from "./assets/pages/Booking/CreateBooking";
+import EditBooking from "./assets/pages/Booking/EditBooking";
+import ReadOneBooking from "./assets/pages/Booking/ReadoneBooking";
+
 
 const App = () => {
   return(
