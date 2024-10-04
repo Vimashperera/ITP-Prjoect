e4d98795f89f91ace909dd60a9e2e370c2650892
ImeshLiyanaarachchi <imesh44445555@gmail.com>
2024-10-04 09:02:25 +0530

    Added the routes for the vehicle and service history module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,44 @@ import CreateBooking from "./assets/pages/Booking/CreateBooking";
 import EditBooking from "./assets/pages/Booking/EditBooking";
 import ReadOneBooking from "./assets/pages/Booking/ReadoneBooking";
 
+import { CreateVehicle } from "./assets/pages/Vehicle/Createvehicle";
+import ShowAllvehicle from "./assets/pages/Vehicle/ShowAllvehicle";
+import EditVehicle from "./assets/pages/Vehicle/EditVehicle";
+import ReadOneVehicle from "./assets/pages/Vehicle/ReadoneVehicle";
+import DeleteVehicle from "./assets/pages/Vehicle/DeleteVehicle";
+
+import CreateServiceHistory from "./assets/pages/ServiceHistory/CreateServiceHistory";
+import EditShowHistory from "./assets/pages/ServiceHistory/EditShowHistory";
+import DeleteShowHistory from "./assets/pages/ServiceHistory/DeleteShowHistory";
+import ReadoneShowHistory from "./assets/pages/ServiceHistory/ReadoneShowHistory";
+import ShowAllServiceHistory from "./assets/pages/ServiceHistory/ShowAllServiceHistory";
 
 const App = () => {
-  return(
-    <div>Monawada load karala balanne </div>
-  )
-}
+  return (
+    <Routes>
+      <Route path="/ServiceHistory/create" element={<CreateServiceHistory />} />
+      <Route path="/ServiceHistory/edit/:id" element={<EditShowHistory />} />
+      <Route
+        path="/ServiceHistory/delete/:id"
+        element={<DeleteShowHistory />}
+      />
+      <Route path="/ServiceHistory/:id" element={<ReadoneShowHistory />} />
+      <Route path="/ServiceHistory" element={<ShowAllServiceHistory />} />
+
+
+      <Route path="/vehicles" element={<ShowAllvehicle />} />
+      <Route path="/vehicles/edit/:id" element={<EditVehicle />} />
+      <Route path="/vehicles/:id" element={<ReadOneVehicle />} />
+      <Route path="/vehicles/delete/:id" element={<DeleteVehicle />} />
+      <Route path="/vehicles/create" element={<CreateVehicle />} />
+      <Route path="/vehicles" element={<ShowAllvehicle />} />
+      <Route path="/vehicles/edit/:id" element={<EditVehicle />} />
+      <Route path="/vehicles/:id" element={<ReadOneVehicle />} />
+      <Route path="/vehicles/delete/:id" element={<DeleteVehicle />} />
+
+
+    </Routes>
+  );
+};
 
 export default App
