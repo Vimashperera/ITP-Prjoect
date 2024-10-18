54b5af11590be964b0c31aefe316dee1b2167e20
WimukthiDilshan <wimukthi2010@gmail.com>
2024-10-18 17:05:37 +0530

    Update ReadOneVacancy.jsx

--- frontend/src/assets/pages/Vacancy/ReadOneVacancy.jsx
+++ frontend/src/assets/pages/Vacancy/ReadOneVacancy.jsx
@@ -1,72 +1,168 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import BackButton from '../../components/BackButton';
 import { useParams } from 'react-router-dom';
 import Spinner from '../../components/Spinner';
-import backgroundImage from '../../images/mee.jpg'; // Ensure this path is correct
+import backgroundImage from '../../images/mee.jpg';
 import Navbar from '../Navbar/Navbar'
 import Footer from '../footer/Footer'
-const ReadOneVacancy = () => {
-  // State initialization
-  const [vacancy, setVacancy] = useState(null); 
-  const [loading, setLoading] = useState(false); 
-  const { id } = useParams(); 
 
-  // Fetch the vacancy data when the component mounts or when 'id' changes
+const ReadOneApplicant = () => {
+  const [applicant, setApplicant] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [typewriterText, setTypewriterText] = useState(""); // State for typewriter effect
+  const { id } = useParams();
+  const [interviewdate, setinterviewdate] = useState('');
+
   useEffect(() => {
-    setLoading(true); 
+    setLoading(true);
     axios
-      .get(`http://localhost:8077/vacancy/${id}`) 
+      .get(`http://localhost:8077/applicant/${id}`)
       .then((response) => {
-        setVacancy(response.data); 
-        setLoading(false); 
+        setApplicant(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
-        setLoading(false); 
+        setLoading(false);
       });
   }, [id]);
 
-  // If the data is still loading, show a spinner
+  const handleReply = () => {
+    if (applicant && applicant.Email) {
+      const email = applicant.Email;
+      const subject = 'Reply to your Application';
+      const jobType = applicant.JobType || 'the position';  // Fallback in case JobType is unavailable
+
+
+      const body = `Dear Customer,
+  
+  Thank you for your application for ${jobType}.
+  
+  We received your application. Your interview date is ${interviewdate}.
+  
+  Best regards,
+  Wasana Service Center`;
+
+      // Open Gmail with pre-filled fields using mailto
+      window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    } else {
+      console.log("Email not available.");
+    }
+  };
+
+  useEffect(() => {
+    const words = ["Show Applicant"];
+    let i = 0;
+    let j = 0;
+    let currentWord = "";
+    let isDeleting = false;
+
+    function type() {
+      currentWord = words[i];
+      if (isDeleting) {
+        setTypewriterText(currentWord.substring(0, j - 1));
+        j--;
+        if (j === 0) {
+          isDeleting = false;
+          i++;
+          if (i === words.length) {
+            i = 0;
+          }
+        }
+      } else {
+        setTypewriterText(currentWord.substring(0, j + 1));
+        j++;
+        if (j === currentWord.length) {
+          isDeleting = true;
+        }
+      }
+      setTimeout(type, 300);
+    }
+
+    type();
+  }, []);
+
   if (loading) {
     return <Spinner />;
   }
 
-  // If the vacancy data hasn't loaded yet, show a loading message
-  if (!vacancy) {
-    return <div className="text-center text-gray-500">No vacancy found.</div>;
+  if (!applicant) {
+    return <p>Data is loading...</p>;
   }
 
   return (
-    <div className=''><Navbar/>
-    <div 
-      className="flex flex-col items-center min-h-screen p-4 bg-center bg-cover" 
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <BackButton destination={`/vacancy/`} />
-      <div className="w-full max-w-xl bg-white rounded-lg shadow-lg p-6 mt-[10%]">
-        <h1 className="mb-6 text-3xl font-semibold text-center text-gray-800">
-          Vacancy Details
-        </h1>
-        <div className="space-y-4">
-          <div className="flex items-center">
-            <span className="w-48 font-semibold text-gray-700">Job ID:</span>
-            <span className="text-gray-600">{vacancy._id}</span>
-          </div>
-          <div className="flex items-center">
-            <span className="w-48 font-semibold text-gray-700">Name:</span>
-            <span className="text-gray-600">{vacancy.Name}</span>
-          </div>
-          <div className="flex items-center">
-            <span className="w-48 font-semibold text-gray-700">Job Description:</span>
-            <span className="text-gray-600">{vacancy.Description}</span>
+    <div className=''><Navbar />
+      <div
+        className="flex flex-col items-center min-h-screen p-4 bg-center bg-cover"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <BackButton destination={`/applicant/`} />
+        <div className="flex items-center justify-center w-full h-full mb-6">
+          <h1 className="text-4xl font-bold text-white">{typewriterText}</h1>
+        </div>
+        <div className="max-w-md mx-auto bg-white rounded-lg shadow-lg hover:shadow-red-800">
+          {applicant.image && (
+            <p>
+              CV:{' '}
+              <a href={applicant.image} target="_blank" rel="noopener noreferrer">
+                <strong>View cv:</strong>
+              </a>
+            </p>
+          )}
+          <div className="px-6 py-6 space-y-4 bg-white rounded-lg shadow-lg">
+            <div className="pb-2 text-2xl font-bold text-gray-800 border-b border-gray-200">Applicant Details</div>
+            <div className="flex flex-col space-y-2">
+              <div className="flex items-center justify-between text-lg font-semibold text-gray-700">
+                <span>Applicant ID:</span>
+                <span className="font-medium text-gray-900">{applicant._id}</span>
+              </div>
+              <div className="flex items-center justify-between text-lg font-semibold text-gray-700">
+                <span>First Name:</span>
+                <span className="font-medium text-gray-900">{applicant.FirstName}</span>
+              </div>
+              <div className="flex items-center justify-between text-lg font-semibold text-gray-700">
+                <span>Last Name:</span>
+                <span className="font-medium text-gray-900">{applicant.LastName}</span>
+              </div>
+              <div className="flex items-center justify-between text-lg font-semibold text-gray-700">
+                <span>Phone Number:</span>
+                <span className="font-medium text-gray-900">{applicant.Number}</span>
+              </div>
+              <div className="flex items-center justify-between text-lg font-semibold text-gray-700">
+                <span>Email:</span>
+                <span className="font-medium text-gray-900">{applicant.Email}</span>
+              </div>
+
+              <div className="flex items-center justify-between text-lg font-semibold text-gray-700">
+                <span>Job Type:</span>
+                <span className="font-medium text-gray-900">{applicant.JobType}</span>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between text-lg font-semibold text-gray-700">
+              <label htmlFor="interviewDate">Interview Date:</label>
+              <input
+                id="interviewDate"
+                type="datetime-local"
+                placeholder="interview date"
+                value={interviewdate}
+                onChange={(e) => setinterviewdate(e.target.value)}
+                className="p-2 font-medium text-gray-900 border border-gray-300 rounded"
+              />
+            </div>
+            <button
+              onClick={handleReply}
+              className="px-4 py-2 mt-6 font-bold text-white bg-green-500 rounded shadow hover:bg-green-600"
+            >
+              Reply
+            </button>
           </div>
         </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </div>
   );
 };
 
-export default ReadOneVacancy;
+export default ReadOneApplicant;
